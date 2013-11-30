64d67725fb9501cf9ee4215a9aee5e53415d9848
drac0 <josehelps@gmail.com>
2013-11-30 15:57:09 -0500

    added more functions to views

--- submarine_app/submarine_app/views.py
+++ submarine_app/submarine_app/views.py
@@ -1,5 +1,31 @@
 from submarine_app import app
 from flask import render_template, redirect, request, current_app, session, flash, url_for
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+@app.route('/', methods=["GET","POST"])
+def index(function=None):
+    error = []
+    return render_template('index.html', error=error)
+
+@app.route('/up')
+def up():
+    error = []
+    print "up was processed."
+    return render_template('index.html', error=error)
+
+@app.route('/down')
+def down():
+    error = []
+    print "down was processed"
+    return render_template('index.html', error=error)
+
+@app.route('/left')
+def left():
+    error = []
+    print "left was processed"
+    return render_template('index.html', error=error)
+
+@app.route('/right')
+def right():
+    error = []
+    print "right was processed"
+    return render_template('index.html', error=error)
