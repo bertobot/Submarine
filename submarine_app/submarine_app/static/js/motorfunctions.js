f1b7931528c657cdee5a8d0c22cd36de64a00d80
drac0 <josehelps@gmail.com>
2013-11-30 15:51:21 -0500

    added directional functions to views, modified templates, also updated motorfunctions.js

---
+++ submarine_app/submarine_app/static/js/motorfunctions.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+//////////////////Motor Functions///////////////////////
+$("#MOTORup").mousehold(300,function(i){
+$.get("http://127.0.0.1:5000/up");
+});
+
+
+});
+
