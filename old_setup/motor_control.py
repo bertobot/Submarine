e562a8642f289dd413ec85fdffaa752ecc9a166c
drac0 <drac0@drac0-desktop.(none)>
2014-01-11 15:34:43 -0500

    modified UI

--- old_setup/motor_control.py
+++ old_setup/motor_control.py
@@ -22,6 +22,10 @@ servoMin = 165  # Min pulse length out of 4096
 servoMax = 600  # Max pulse length out of 4096
 servoReverse = 300
 
+vertical_servo_pin = 0
+left_servo_pin = 1
+right_servo_pint = 2
+
 def setServoPulse(channel, pulse):
   pulseLength = 1000000                   # 1,000,000 us per second
   pulseLength /= 60                       # 60 Hz
--- submarine_app/views.py
+++ submarine_app/views.py
@@ -1,5 +1,46 @@
 from submarine_app import app
 from flask import render_template, redirect, request, current_app, session, flash, url_for
+from Adafruit_PWM_Servo_Driver import PWM
+import time
+import sys
+import re
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.parse_args()
+
+
+# ===========================================================================
+# Example Code
+# ===========================================================================
+
+# Initialise the PWM device using the default address
+# bmp = PWM(0x40, debug=True)
+pwm = PWM(0x40, debug=True)
+
+servoMin = 400  # Min pulse length out of 4096
+servoMax = 440  # Max pulse length out of 4096
+servoReverse = 440
+
+vertical_pin = 0
+left_pin = 1
+right_pin = 2
+
+
+def setServoPulse(channel, pulse):
+  pulseLength = 1000000                   # 1,000,000 us per second
+  pulseLength /= 60                       # 60 Hz
+  print "%d us per period" % pulseLength
+  pulseLength /= 4096                     # 12 bits of resolution
+  print "%d us per bit" % pulseLength
+  pulse *= 1000
+  pulse /= pulseLength
+  pwm.setPWM(channel, 0, pulse)
+
+pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+
+
+
 
 @app.route('/', methods=["GET","POST"])
 def index(function=None):
