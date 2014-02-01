d85906a9061f17cb14f7dca63384ded323efed58
berto <berto@lobosunidos.org>
2014-02-01 11:28:32 -0500

    added logic to handle excess packet(s) when stop (0 0 0) is called.

--- submarine_server/SubmarineUDPServer.py
+++ submarine_server/SubmarineUDPServer.py
@@ -6,11 +6,26 @@ class SubmarineUDPServer:
 	def __init__(self, port):
 		self.submarine = SubmarineMovement()
 		self.udpServer = UDPServer(port, self.ourCallback)
+		self.toss = False;
 
 	def ourCallback(data, addr):
+		if (self.toss):
+			self.toss = False
+			return
+
 		print "received: ", data
+
 		whitespace = "\\s"
 		self.submarine.move(data.split(whitespace))
+
+		values = data.split(whitespace)
+
+		if (values[0] == values[1] and values[1] == values[2] and values[2] == 0):
+			self.toss = True
+
+		self.submarine.move(values)
+
+		
 
 	def start(self):
 		self.udpServer.start()
